be9746ca5faba8ec8ad18d0676403a3ffffed882
AnandSingh-0619 <anandsingh16898@gmail.com>
2024-04-24 14:32:28 -0400

    new added

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=home-robot-yolo
-#SBATCH --output=slurm_logs/home_robo-ver-%j.out
-#SBATCH --error=slurm_logs/home_robo-ver-%j.err
+#SBATCH --output=Logs/slurmLogs/home_robo-ver-%j.out
+#SBATCH --error=Logs/slurmLogs/home_robo-ver-%j.err
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 10
 #SBATCH --ntasks-per-node 4
@@ -12,31 +12,24 @@
 #SBATCH --requeue
 #SBATCH --signal=USR1@100
 
+export HABITAT_SIM_LOG=quiet
+export MAGNUM_LOG=quiet
 export PYTHONPATH=~/flash/home-robot/projects/:$PYTHONPATH
-<<<<<<< Updated upstream
-CHECKPOINT_DIR="data/new_checkpoints/ovmm/gaze_og_heatmap4"
-TENSORBOARD_DIR="tb/yolo_gaze/nGPU4_nENV32_og_heatmap4"
-=======
-export CUDA_LAUNCH_BLOCKING=1
-CHECKPOINT_DIR="data/new_checkpoints/ddppo/ovmm/gaze_yolo_heatmap1"
-TENSORBOARD_DIR="tb/yolo_gaze/nGPU4_nENV32_yolo_heatmap1"
->>>>>>> Stashed changes
+
+MAIN_ADDR=$(scontrol show hostnames "
+${SLURM_JOB_NODELIST}" | head -n 1)
+export MAIN_ADDR
+
+CHECKPOINT_DIR="Logs/checkpoints/gaze/gaze_og"
+TENSORBOARD_DIR="Logs/tensorLogs/nGPU4_nENV32/gaze/gaze_og"
+LOG_DIR="Logs/logs/gaze/gaze_og.log"
+
 source ~/.bashrc
 source /nethome/asingh3064/flash/miniforge3/etc/profile.d/conda.sh
 
 conda deactivate
 conda activate home-robot
-# export HABITAT_ENV_DEBUG=1
 cd ~/flash/home-robot 
-
-# srun python -um habitat_uncertainity.run \
-#     --exp-config=projects/habitat_uncertainity/config/yolo_rl_skill.yaml \
-#     --run-type=train \
-#     habitat_baselines.num_environments=32 \
-#     habitat_baselines.tensorboard_dir=${TENSORBOARD_DIR} \
-#     habitat_baselines.checkpoint_folder=${CHECKPOINT_DIR} \
-#     habitat_baselines.load_resume_state_config=True \
-
 
 srun python -um habitat_baselines.run \
    --config-name=ovmm/rl_skill.yaml \
@@ -44,4 +37,5 @@ srun python -um habitat_baselines.run \
    habitat_baselines.num_environments=32 \
    habitat_baselines.tensorboard_dir=${TENSORBOARD_DIR} \
    habitat_baselines.checkpoint_folder=${CHECKPOINT_DIR} \
-   habitat_baselines.load_resume_state_config=False \
+   habitat_baselines.log_file=${LOG_DIR} \
+   habitat_baselines.load_resume_state_config=True \
